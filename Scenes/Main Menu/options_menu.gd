8bad77ed061b031e5feda0c8b8fc066b0121b20c
domm777 <mnetsereab8473@conestogac.on.ca>
2024-10-22 10:37:46 -0400

    Basic Main Menu Done

--- Scenes/Main Menu/options_menu.gd
+++ Scenes/Main Menu/options_menu.gd
@@ -1,4 +1,7 @@
+class_name Main_menu
 extends Control
-
+func _on_volume_pressed() -> void:
+	pass # Replace with function body.
+	
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Main Menu/Main_menu.tscn")
