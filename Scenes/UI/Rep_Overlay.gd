291dd584a778dc2eeb475bde447115ee3f5e86fa
Caleb <flyingmigit8@hotmail.com>
2024-10-22 20:47:26 -0400

    Reputation overlay

---
+++ Scenes/UI/Rep_Overlay.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var reputation_label = $ReputationLabel # Assuming the Label node is named "ReputationLabel"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	update_ui()  # Update UI every frame (optional)
+
+#func update_ui():
+	# Access global variables from global.gd
+	#var initialText = "Reputation: "
+	#reputation_label.text = str(initialText + Global.reputation)
