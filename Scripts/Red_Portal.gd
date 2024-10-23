517d49b1fd3c15c10a3ab819da33ffa1d562d840
Caleb <flyingmigit8@hotmail.com>
2024-10-23 19:42:38 -0400

    enabled level 1 through the portal

--- Scripts/Red_Portal.gd
+++ Scripts/Red_Portal.gd
@@ -3,8 +3,20 @@ var target_scene = "res://Scenes/Levels/Level_Selection.tscn" # Scene to transit
 
 var scene_change_triggered = false  # Flag to track if scene change has been triggered
 
-#func _ready() -> void:
-	#print("Portal Ready!") # Debugging: Check if the script is running 
+func _ready() -> void:
+	match Global.portals_entered:
+		0:
+			target_scene = "res://Scenes/Levels/Level_1.tscn"
+		1:
+			target_scene = "res://Scenes/Levels/Level_2.tscn"
+		2:
+			target_scene = "res://Scenes/Levels/Level_3.tscn"
+		3:
+			target_scene = "res://Scenes/Levels/Level_4.tscn"
+		_:
+			target_scene = "res://Scenes/Levels/Default_Level.tscn" # Fallback in case of unexpected value
+
+	print("Target Scene:", target_scene)
 
 func _on_body_entered(body) -> void:
 	if body.is_in_group("player"):
@@ -18,10 +30,12 @@ func _on_body_exited(body) -> void:
 func _next_scene() -> void:
 	var player_node = get_tree().get_nodes_in_group("player")[0]
 	if player_node and not scene_change_triggered:
-		scene_change_triggered = true  # Set the flag to prevent further calls
+		if Global.key_fragment_count > Global.portals_entered:
+			scene_change_triggered = true  # Set the flag to prevent further calls
 		
-		# Access global variable
-		Global.reputation -= 1
+			# Access global variable
+			Global.reputation -= 1
+			Global.portals_entered -= 1
 
-		print("PLAYER REPUTATION UPDATED:", Global.reputation) 
-		get_tree().change_scene_to_file(target_scene)
+			print("PLAYER REPUTATION UPDATED:", Global.reputation) 
+			get_tree().change_scene_to_file(target_scene)
