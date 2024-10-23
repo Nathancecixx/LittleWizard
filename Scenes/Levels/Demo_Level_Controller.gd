2666af91d9e4fe6b4db29076bc4272a7c4a33bfc
domm777 <mnetsereab8473@conestogac.on.ca>
2024-10-23 16:27:06 -0400

    Added 1-player

--- Scenes/Levels/Demo_Level_Controller.gd
+++ Scenes/Levels/Demo_Level_Controller.gd
@@ -9,3 +9,6 @@ func _ready():
 	
 	# Pass the parameter (e.g., speed)
 	player.Set_Clamp(Level_Width, Level_Height)
+
+func _on_pause_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main Menu/Options_menu.tscn")
