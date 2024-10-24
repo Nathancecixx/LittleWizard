ba8010df6358b4993e72b8772d6f61d20dc715d0
Andrew Abrantes <aabrantes4064@conestogac.on.ca>
2024-10-23 18:57:20 -0700

    Selection Door added and QoL
    Wizard now moves aftercasting a fireball while holding a direction and there is a door leading to level 1 in level selection

--- Scripts/Levels/level_selection.gd
+++ Scripts/Levels/level_selection.gd
@@ -5,9 +5,18 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.Set_Clamp(2560, 10000)
-	pass # Replace with function body.
+	$Level_1_Door_Interaction.timeline = "level_1_interact"
+	$Level_1_Door_Interaction.remove_timeline_signal = "null"
+	$test_dialog.timeline = "test_timeline"
+	$test_dialog.remove_timeline_signal = "end_test"
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_dialogic_signal(argument: String):
+
+	if (argument=="enter_level_1"):
+		get_tree().change_scene_to_file("res://Scenes/Levels/Level_1.tscn")
