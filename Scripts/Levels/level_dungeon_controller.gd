3167b9bdd0e75d16df848d955edd0ce731a096fa
Andrew Abrantes <aabrantes4064@conestogac.on.ca>
2024-10-24 17:55:04 -0700

    NPCs
    Added NPCs at the level selection and in each level, dialog options can effect reputation

--- Scripts/Levels/level_dungeon_controller.gd
+++ Scripts/Levels/level_dungeon_controller.gd
@@ -9,9 +9,17 @@ func _ready() -> void:
 	player.Set_Clamp(6400, 1600)
 	respawn = player.global_position
 	Global.level_2_complete = true
+	$NPC_Dialog.timeline = "bug2_npc"
+	$NPC_Dialog.remove_timeline_signal = "null"
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+func _on_dialogic_signal(argument: String):
+
+	if (argument=="end_dialog"):
+		$NPC_Dialog.timeline = "bug2_retalk"
